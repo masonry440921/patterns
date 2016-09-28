cf1bd5eff481b4946af00fcd49b10b1485a21f6b
mason <free-masonry@hotmail.com>
2016-09-28 12:05:27 +0800

    dfa

--- src/visitor/example_1/App.java
+++ src/visitor/example_1/App.java
@@ -1,6 +1,8 @@
 package visitor.example_1;
 
 public class App {
+	
+	//核心数据基础类有个方法接受访问者，将自身引用转给访问这，accept(visitor  v){v.visit(this)}
 	public static void main(String[] args) {
 		ComputerPart computer = new Computer();
 	    computer.accept(new ComputerPartDisplayVisitor());
